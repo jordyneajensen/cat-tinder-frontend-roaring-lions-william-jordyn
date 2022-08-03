e6826d9395a80841ffde1ae8d972e13821733a4f
Jordyn Jensen <jordyneajensen@gmail.com>
2022-08-03 11:58:03 -0700

    fixed not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import notfoundlion from '../assets/notfoundlion.jpg'
+import notfoundlion from '../assets/notfoundlion.png'
 
 class NotFound extends Component {
   render() {
     return (
       <>
       <div className="not-found">
-        <h1>Being Brave Doesn't Mean You Go looking For Trouble -- Mufasa</h1>
         <img className="lion-not-found" src={notfoundlion}/>
       </div>
       </>
